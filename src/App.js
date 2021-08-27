85fb7295f09dfead45fe7b57e8e415e198486ea0
Ali <aifares@buffalo.edu>
2021-08-27 00:33:02 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Typewriter from "typewriter-effect";
 import NavBar from "./NavBar";
 import { Button } from "@material-ui/core";
-import pdf from "./Resume.pdf";
+import pdf from "./Resume_Ali_Fares.pdf";
 import FadeIn from "react-fade-in";
 
 function App() {
@@ -29,8 +29,10 @@ function App() {
             <span>About Me</span>
           </h2>
         </div>
+
         <div className="about-me-info">
           <FadeIn>
+            <p></p>
             <p>Hi! My name is Ali Fares</p>
             <p>I am a aspiring Software Engineer</p>
             <p>with a natural affinty for building products!</p>
@@ -38,10 +40,10 @@ function App() {
         </div>
         <FadeIn>
           <div className="about-me-info2">
-            <p>I am currently a junior at the Univeristy at Buffalo</p>
+            <p>I am currently a senior at the Univeristy at Buffalo</p>
             <p>I love sports,sneakers and coding</p>
-            <p>I currently have a Software Engineering Internship</p>
-            <p>with American Express for Summer 2021.</p>
+            <p>I currently just finished a internship</p>
+            <p>with American Express</p>
           </div>
         </FadeIn>
         <a name="resume"></a>
@@ -84,9 +86,9 @@ function App() {
         <div className="skills">
           <ul className="bullet">
             <li>
-              Experienced in Python, Javascript, Scala, MySQL, C, Django, React,
-              Node.js, CSS, Web Sockets, and  HTML, Pandas, NumPy, MongoDB,
-              Express.
+              Experienced in Python, JavaScript, Scala, MySQL, C, Django, React,
+              Node.js, CSS, Web Sockets HTML, Pandas, NumPy, MongoDB, Express,
+              GitHub, Zen-Hub, Fastify, Electron, PostgreSQL.
             </li>
             <li>
               Solution-oriented and a problem solver, with a high level of
@@ -107,6 +109,38 @@ function App() {
         </div>
         <div className="section-header">
           {" "}
+          <u> Technical Work Experience</u>{" "}
+        </div>
+        <div className="tech-projects">
+          <div className="tech-projects">
+            <b>Software Engineering Intern, American Express</b>{" "}
+            <ul>
+              <li>
+                Created a dashboard using React.js to display data regarding the
+                use of each component of an internal library.
+              </li>
+              <li>
+                Using Fastify.js, Node.js and PostgreSQL created an API to send
+                and receive data from the PostgreSQL Database.
+              </li>
+              <li>
+                Developed and designed a full system architecture plan for the
+                project.
+              </li>
+              <li>
+                Using Electron and SQLite3, migrated the existing web
+                application to a local native desktop application.
+              </li>
+              <li>
+                Held daily stand-ups with the team and mentors to provide
+                updates on the project and ensure all deadlines are met.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="section-header">
+          {" "}
           <u> Technical Projects</u>{" "}
         </div>
         <div className="tech-projects">
@@ -114,116 +148,112 @@ function App() {
             <b>Raul Avila Inc. E-Commerce Website</b>{" "}
             <ul>
               <li>
-                Using Python’s Django framework created a backend which included
-                a user authentication system, inventory management and payment
-                processor though Stripe’s API.
+                Utilizing Python’s Django framework created a backend that
+                included a user authentication system, inventory management, and
+                integrated payment processor through Stripe.
               </li>
               <li>
                 Effectively communicated with the client and team members to
                 deliver the product and meet deadlines.
               </li>
               <li>
-                Using HTML and CSS created a user fronted and employee dashboard to allow employees
-                to update products.
-              </li>
-            </ul>
-          </div>
-          <div className="tech-projects">
-            <b>Trie Dictionary</b>
-            <ul>
-              <li>
-                Created a user dictionary in Scala using a trie graph data
-                structure where a user can analyze words and associate each word
-                with a value or information.
-              </li>
-              <li>
-                Using the slightly modified breadth-first search algorithm, was
-                able to achieve O(k) where k is the length of the word runtime
-                when finding words and their values.
-              </li>
-              <li>Was able to parse thorough text and analyze each word.</li>
-            </ul>
-          </div>
-          <div className="tech-projects">
-            <b>UB Switch Project Cost of Living Analyzer</b>
-            <ul>
-              <li>
-                Using React, Node.js, Express and MongoDB created a full stack
-                website to retrieve data from a MongoDB database and analyze the
-                data.
-              </li>
-              <li>
-                Using Canvas I was able to create visualizations of the data to
-                show users.
-              </li>
-              <li>
-                Created a fully functional authentication system which using
-                Firebase and Node.js.
+                Using HTML and CSS created a dynamic frontend to allow employees
+                to make changes to the website.
               </li>
             </ul>
           </div>
         </div>
-        <div className="button-download">
-          <Button
-            style={{
-              backgroundColor: "#3066BE",
-            }}
-            href={pdf}
-            target="_blank"
-            variant="contained"
-            color="primary"
-          >
-            {" "}
-            <h3>Download Resume</h3>
-          </Button>
+        <div className="tech-projects">
+          <b>UB Switch Project Cost of Living Analyzer</b>
+          <ul>
+            <li>
+              Using Node.js, Express and MongoDB created an API with multiple
+              endpoints to retrieve a variety of data from the MongoDB database.
+            </li>
+            <li>
+              Created a fully functional authentication system that used
+              Google's Firebase and Node.js.
+            </li>
+            <li>
+              Created a fully functional authentication system that used
+              Google's Firebase and Node.js.
+            </li>
+          </ul>
         </div>
-        <a name="contact"></a>
-
-        <div className="divder">
-          <h2>
-            <span>Contact</span>
-          </h2>
-        </div>
-        <div className="section-header-contact">
+        <div className="section-header">
           {" "}
-          Connect with me on Social Media{" "}
+          <u>Leadership Experience and Activites</u>{" "}
         </div>
-
-        <Button
-          href="https://www.linkedin.com/in/alifaresalifares/"
-          style={{
-            backgroundColor: "#3066BE",
-          }}
-          variant="contained"
-          color="primary"
-        >
-          LinkedIn
-        </Button>
-        <div class="button-divide" />
-        <Button
-          href="https://www.github.com/aifares"
-          style={{
-            backgroundColor: "#3066BE",
-          }}
-          variant="contained"
-          color="primary"
-        >
-          GitHub
-        </Button>
-        <div class="button-divide" />
-        <Button
-          href="mailto:aifares@icloud.com"
-          style={{
-            backgroundColor: "#3066BE",
-          }}
-          variant="contained"
-          color="primary"
-        >
-          Email
-        </Button>
-        <div className="spacer"></div>
-        <div className="section-header-contact"> </div>
+        <div className="tech-projects">
+          <li>
+            {" "}
+            <p>
+              <b>Member</b>,Society of Hispanic Engineers (SHPE), September 2019
+              - present
+            </p>
+          </li>
+        </div>
       </div>
+      <div className="button-download">
+        <Button
+          style={{
+            backgroundColor: "#3066BE",
+          }}
+          href={pdf}
+          target="_blank"
+          variant="contained"
+          color="primary"
+        >
+          {" "}
+          <h3>Download Resume</h3>
+        </Button>
+      </div>
+      <a name="contact"></a>
+
+      <div className="divder">
+        <h2>
+          <span>Contact</span>
+        </h2>
+      </div>
+      <div className="section-header-contact">
+        {" "}
+        Connect with me on Social Media{" "}
+      </div>
+
+      <Button
+        href="https://www.linkedin.com/in/alifaresalifares/"
+        style={{
+          backgroundColor: "#3066BE",
+        }}
+        variant="contained"
+        color="primary"
+      >
+        LinkedIn
+      </Button>
+      <div class="button-divide" />
+      <Button
+        href="https://www.github.com/aifares"
+        style={{
+          backgroundColor: "#3066BE",
+        }}
+        variant="contained"
+        color="primary"
+      >
+        GitHub
+      </Button>
+      <div class="button-divide" />
+      <Button
+        href="mailto:aifares@icloud.com"
+        style={{
+          backgroundColor: "#3066BE",
+        }}
+        variant="contained"
+        color="primary"
+      >
+        Email
+      </Button>
+      <div className="spacer"></div>
+      <div className="section-header-contact"> </div>
     </div>
   );
 }
